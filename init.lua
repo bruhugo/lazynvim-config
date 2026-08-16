a1678e23ffd52468927f1b2eeeb03ca9c4a792a3
Bruno Hugo <brunohugo2007@gmail.com>
2026-08-16 20:05:03 -0300

    first files

---
+++ init.lua
@@ -0,0 +1,15 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
